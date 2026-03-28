3a373d33d7889079ab2a1838595e5594358bb52f
avi-codesmith <avi.codesmith@gmail.com>
2026-03-28 18:19:32 +0530

    Client side components VS Server Side components

---
+++ components/main-header/main-header.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import logo from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logo} alt="Logo" priority />
+          Next level food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
