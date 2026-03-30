902ee08a7411fa0b7e9e1926e44f6f469ebd00c2
avi-codesmith <avi.codesmith@gmail.com>
2026-03-30 17:15:28 +0530

    Add meals by using SQLite

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,6 +1,13 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
   return (
@@ -18,7 +25,9 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense fallback="Loading...">
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
