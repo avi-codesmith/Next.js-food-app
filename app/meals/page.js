f36621206328fedd8f49761480fc1f0d00adf467
avi-codesmith <avi.codesmith@gmail.com>
2026-03-30 16:11:21 +0530

    Add SQLite database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
   return (
@@ -16,7 +17,9 @@ export default function MealsPage() {
           <Link href={"/meals/share"}>Share Your Favorite Recipes</Link>
         </p>
       </header>
-      <main className={classes.main}></main>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
