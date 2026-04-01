d651796a8a1473a9dcac3b6ed0c61b03e2269c49
avi-codesmith <avi.codesmith@gmail.com>
2026-04-01 17:13:20 +0530

    Add server side validation

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,13 @@
+"use client";
+
 import ShareMeal from "@/lib/actions";
 import ImagePicker from "./image-picker";
 import classes from "./page.module.css";
+import FormButton from "@/components/meals/form-button";
+import { useFormState } from "react-dom";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(ShareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -12,24 +17,24 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={ShareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
+              <input type="text" id="name" name="name" />
             </p>
             <p>
               <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
+              <input type="email" id="email" name="email" />
             </p>
           </div>
           <p>
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" required />
+            <input type="text" id="title" name="title" />
           </p>
           <p>
             <label htmlFor="summary">Short Summary</label>
-            <input type="text" id="summary" name="summary" required />
+            <input type="text" id="summary" name="summary" />
           </p>
           <p>
             <label htmlFor="instructions">Instructions</label>
@@ -37,12 +42,12 @@ export default function ShareMealPage() {
               id="instructions"
               name="instructions"
               rows="10"
-              required
             ></textarea>
           </p>
+          <p>{state?.message}</p>
           <ImagePicker label={"your image"} name={"image"} />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <FormButton />
           </p>
         </form>
       </main>
